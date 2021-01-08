334d2d712bb9fea70506f7c241b1c0c295cf548f
Oskar <oskarbrudnowski@gmail.com>
2021-01-08 08:18:43 +0100

    angular cli update

--- src/app/shared/components/add-subtask-pop-up/add-subtask-pop-up.component.spec.ts
+++ src/app/shared/components/add-subtask-pop-up/add-subtask-pop-up.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('AddSubtaskPopUpComponent', () => {
   let component: AddSubtaskPopUpComponent;
   let fixture: ComponentFixture<AddSubtaskPopUpComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddSubtaskPopUpComponent ]
     })
